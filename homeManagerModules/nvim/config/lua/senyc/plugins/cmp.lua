912e73415ba775fa92bc763a88be0e05dbf61a9c
senyc <95313103+senyc@users.noreply.github.com>
2024-05-05 23:56:46 -0400

    feat: update desktop host generation to 24 on 2024-05-05

--- homeManagerModules/nvim/config/lua/senyc/plugins/cmp.lua
+++ homeManagerModules/nvim/config/lua/senyc/plugins/cmp.lua
@@ -41,7 +41,7 @@ return {
         ['<tab>'] = cmp.mapping.confirm {
           select = true,
         },
-        ['<C-p>'] = cmp.mapping(function(fallback)
+        ['<C-n>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
@@ -50,7 +50,7 @@ return {
             fallback()
           end
         end, { 'i', 's' }),
-        ['<C-n>'] = cmp.mapping(function(fallback)
+        ['<C-p>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
