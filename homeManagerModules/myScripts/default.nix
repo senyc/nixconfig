1baa87e5c4b38296a000ec0f2d56b885fd5fbf7b
senyc <95313103+senyc@users.noreply.github.com>
2024-06-01 15:23:21 -0400

    feat: update taliwind sorter commands

--- homeManagerModules/myScripts/default.nix
+++ homeManagerModules/myScripts/default.nix
@@ -57,21 +57,29 @@ with lib; {
       (writeShellScriptBin "screenshot" ''
         ${grim}/bin/grim -g "$(${slurp}/bin/slurp -w 0)" - | ${wl-clipboard}/bin/wl-copy
       '')
+      (writeShellScriptBin "screenshotsave" ''
+        if ! [[ -d "$HOME/Pictures/screenshots/" ]]; then
+            mkdir -p "$HOME/Pictures/screenshots/"
+        fi
+        date_string=$(date +"%Y-%m-%d%H:%M:%S")
+
+        ${grim}/bin/grim -g "$(${slurp}/bin/slurp -w 0)" "$HOME/Pictures/screenshots/$date_string.png"
+      '')
       (writeShellScriptBin "rmdockercontainers" ''
         for i in $(${docker}/bin/docker ps --all | awk '{print $1}' | tail -n +2); do
           ${docker}/bin/docker rm $i
         done
       '')
       (writeShellScriptBin "makekeyfromssh" ''
-       mkdir -p ~/.config/sops/age/
-       nix run nixpkgs#ssh-to-age -- -private-key -i  ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt
-       nix shell nixpkgs#age -c age-keygen -y ~/.config/sops/age/keys.txt
+        mkdir -p ~/.config/sops/age/
+        nix run nixpkgs#ssh-to-age -- -private-key -i  ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt
+        nix shell nixpkgs#age -c age-keygen -y ~/.config/sops/age/keys.txt
       '')
       (writeShellScriptBin "getsecret" ''
-       cat "/var/run/secrets/$1"
+        cat "/var/run/secrets/$1"
       '')
       (writeShellScriptBin "authspotify" ''
-       ${spotify}/bin/spotify --username="$(getsecret spotify-username)" --password="$(getsecret spotify-password)"
+        ${spotify}/bin/spotify --username="$(getsecret spotify-username)" --password="$(getsecret spotify-password)"
       '')
     ];
   };
--- homeManagerModules/nvim/config/lua/senyc/plugins/tailwind-sorter.lua
+++ homeManagerModules/nvim/config/lua/senyc/plugins/tailwind-sorter.lua
@@ -6,12 +6,12 @@ return {
   build = 'cd formatter && npm i && npm run build',
   ft = { 'html', 'js', 'javascriptreact', 'typescriptreact', 'astro', 'ts' },
   config = function()
+    local map = require "senyc.utils".default_map
     require 'tailwind-sorter'.setup({
-
-      on_save_enabled = true,
-      -- The config.js pattern may lead to bugs
-      on_save_pattern = { "!(*.config).js", '*.html', '*.jsx', '*.tsx', '*.astro', '*.html.erb'}, -- The file patterns to watch and sort.
+      on_save_enabled = false,
       node_path = 'node',
     })
+    -- tailwind sort
+    map("n", "<leader>ts", vim.cmd.TailwindSort)
   end
 }
