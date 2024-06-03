07bac72ab66793b5fa5a74016df3bd734d6cc6d1
senyc <95313103+senyc@users.noreply.github.com>
2024-06-02 21:17:16 -0400

    feat: move neovim related packages to only be available to it

--- userModules/nvim/default.nix
+++ userModules/nvim/default.nix
@@ -12,25 +12,23 @@
       enable = true;
       defaultEditor = true;
       vimAlias = true;
+      extraPackages = with pkgs; [
+        clang-tools # Clangd
+        yaml-language-server
+        alejandra
+        lua-language-server
+        nodePackages.typescript-language-server
+        nodePackages.pyright
+        nil
+        gopls
+        tailwindcss-language-server
+        nodePackages.bash-language-server
+      ];
     };
     # Link all of the lua config
     xdg.configFile.nvim = {
       source = ./config;
       recursive = true;
     };
-
-    # Lanuage servers and formatters
-    home.packages = with pkgs; [
-      clang-tools # Clangd
-      yaml-language-server
-      alejandra
-      lua-language-server
-      nodePackages.typescript-language-server
-      nodePackages.pyright
-      nil
-      gopls
-      tailwindcss-language-server
-      nodePackages.bash-language-server
-    ];
   };
 }
