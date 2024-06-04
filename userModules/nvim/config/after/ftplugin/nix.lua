07156c274c2aedfd7f4017a083526cd0698c47d4
senyc <95313103+senyc@users.noreply.github.com>
2024-06-04 17:30:27 -0400

    chore: remove unused ftplugins

--- userModules/nvim/config/after/ftplugin/nix.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
--- vim.bo.commentstring = '# %s'
--- vim.bo.smartindent = false
--- vim.bo.cindent = false
