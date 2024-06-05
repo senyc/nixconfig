2ce7c8f765e726d80b3d3d3fbdd38636f89762b0
senyc <95313103+senyc@users.noreply.github.com>
2024-06-05 19:20:03 -0400

    feat: update alacritty default font to use jet brains

--- userModules/alacritty/default.nix
+++ userModules/alacritty/default.nix
@@ -13,7 +13,23 @@
         env.TERM = "xterm-256color";
         colors.draw_bold_text_with_bright_colors = true;
         font = {
-          size = 12;
+          size = 13;
+          bold = {
+            family = "JetBrainsMono Nerd Font Mono";
+            style = "Bold";
+          };
+          bold_italic = {
+            family = "JetBrainsMono Nerd Font Mono";
+            style = "Bold Italic";
+          };
+          italic = {
+            family = "JetBrainsMono Nerd Font Mono";
+            style = "Italic";
+          };
+          normal = {
+            family = "JetBrainsMono Nerd Font Mono";
+            style = "Regular";
+          };
         };
         window.opacity = 0.50;
         window.padding.x = 5;
