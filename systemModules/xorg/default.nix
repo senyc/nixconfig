2264d6e123ffe17bf04a5be8c3dfab1f94d5e81a
senyc <95313103+senyc@users.noreply.github.com>
2024-06-11 09:36:14 -0400

    feat: add xorg host

---
+++ systemModules/xorg/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  options = {
+    modules.system.xorg.enable = lib.mkEnableOption "Enables xorg-related settings";
+  };
+  config = lib.mkIf config.modules.system.xorg.enable {
+    services.xserver = {
+      enable = true;
+      displayManager.gdm = {
+        enable = true;
+        wayland = false;
+      };
+      desktopManager.gnome.enable = true;
+    };
+    programs.dconf.enable = true;
+    environment.systemPackages = with pkgs; [
+      gnome.adwaita-icon-theme
+      gnomeExtensions.appindicator
+    ];
+    services.udev.packages = with pkgs; [gnome.gnome-settings-daemon];
+  };
+}
