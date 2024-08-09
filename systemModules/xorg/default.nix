5ae0c471b6f11c33878ee81ab3816eb7d6f6ceb9
kyler <95313103+senyc@users.noreply.github.com>
2024-08-09 19:29:55 -0400

    feat: add gnome configurations to alternative host

--- systemModules/xorg/default.nix
+++ systemModules/xorg/default.nix
@@ -16,8 +16,64 @@
       };
       desktopManager.gnome.enable = true;
     };
+    programs.dconf = {
+      enable = true;
+      profiles.user.databases = [
+        {
+          lockAll = true;
+          settings = {
+            "org/gnome/desktop/interface" = {
+              overlay-scrolling = false;
+            };
+            "org/ghome/desktop/peripherals/mouse" = {
+              accel-profile = "flat";
+              natural-scroll = false;
+            };
+            "org/ghome/desktop/peripherals/touchpad" = {
+              two-fonger-scrolling-enabled = true;
+            };
+            "org/gnome/desktop/wm/keybindings" = {
+              switch-to-workspace-1 = ["<Super>e"];
+              switch-to-workspace-2 = ["<Super>r"];
+              switch-to-workspace-3 = ["<Super>o"];
+              switch-to-workspace-4 = ["<Super>m"];
+              toggle-maximized = ["<Super>f"];
+              cycle-windows = ["<Super>h"];
+              cycle-windows-backward = ["<Super>l"];
+              panel-run-dialog = ["<Super>semicolon"];
+              minimize = lib.gvariant.mkEmptyArray lib.gvariant.type.string;
+              close = ["<Super>x"];
+            };
+            "org/gnome/mutter" = {
+              edge-tiling = true;
+            };
+            "org/gnome/mutter/keybindings" = {
+              toggle-tiled-left = ["<Shift><Super>h"];
+              toggle-tiled-right = ["<Shift><Super>l"];
+            };
 
-    programs.dconf.enable = true;
+            "org/gnome/settings-daemon/plugins/media-keys" = {
+              screensaver = lib.gvariant.mkEmptyArray lib.gvariant.type.string;
+              custom-keybindings = [
+                "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+                "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+              ];
+            };
+            "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+              binding = "<Super>q";
+              command = "kitty";
+              name = "Open terminal";
+            };
+
+            "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+              binding = "<Super>b";
+              command = "chromium";
+              name = "Open browser";
+            };
+          };
+        }
+      ];
+    };
     environment.systemPackages = with pkgs; [
       adwaita-icon-theme
       gnomeExtensions.appindicator
