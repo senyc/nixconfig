206e0eabdbefbd3d2f88888ca1a9a08f4d5a66a6
kyler <95313103+senyc@users.noreply.github.com>
2024-11-24 20:33:17 -0500

    feat(sops)!: delete old secret hashes and disable use of sops

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -13,7 +13,6 @@
     "network"
     "packages"
     "users"
-    "sops"
     "containerization"
     "wayland"
     "pipewire"
--- systemModules/sops/default.nix
+++ systemModules/sops/default.nix
@@ -13,23 +13,24 @@ with lib; {
   };
 
   config = mkIf config.modules.system.sops.enable {
-    sops.defaultSopsFile = ../../secrets/secrets.yaml;
-    sops.defaultSopsFormat = "yaml";
-    sops.age.keyFile = "/home/senyc/.config/sops/age/keys.txt";
-
-    sops.age.generateKey = false;
-    # This is the actual specification of the secrets.
-    sops.secrets.spotify-password = {
-      owner = config.users.users.senyc.name;
-      group = config.users.users.senyc.group;
-    };
-    sops.secrets.spotify-username = {
-      owner = config.users.users.senyc.name;
-      group = config.users.users.senyc.group;
-    };
-    sops.secrets.morning-tabs = {
-      owner = config.users.users.senyc.name;
-      group = config.users.users.senyc.group;
+    sops = {
+      defaultSopsFile = ../../secrets/secrets.yaml;
+      defaultSopsFormat = "yaml";
+      age.keyFile = "/home/senyc/.config/sops/age/keys.txt";
+      age.generateKey = false;
+      # This is the actual specification of the secrets.
+      secrets.spotify-password = {
+        owner = config.users.users.senyc.name;
+        group = config.users.users.senyc.group;
+      };
+      secrets.spotify-username = {
+        owner = config.users.users.senyc.name;
+        group = config.users.users.senyc.group;
+      };
+      secrets.morning-tabs = {
+        owner = config.users.users.senyc.name;
+        group = config.users.users.senyc.group;
+      };
     };
   };
 }
