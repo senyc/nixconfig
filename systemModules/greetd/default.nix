2f33a355a3144cd0d1acca849be178e3f4c2aef8
senyc <95313103+senyc@users.noreply.github.com>
2026-01-16 19:02:23 -0500

    feat(greet): boot no longer has text override the greeter

--- systemModules/greetd/default.nix
+++ systemModules/greetd/default.nix
@@ -5,12 +5,20 @@
   ...
 }: let
   tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
-  session = "${pkgs.hyprland}/bin/Hyprland";
+  session = "${pkgs.hyprland}/bin/start-hyprland";
 in {
   options = {
     modules.system.greetd.enable = lib.mkEnableOption "Enable the greeter";
   };
   config = lib.mkIf config.modules.system.greetd.enable {
+    # Silence boot output so it doesn't overlap with greetd
+    boot.kernelParams = [
+      "quiet"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+    ];
+
     services.greetd = {
       enable = true;
       settings = {
